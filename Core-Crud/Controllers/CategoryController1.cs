146185e7d2e2f01d28ad311ed5d63df1dda68e26
bscaria <bscaria@SUYCOKHWDSK223>
2022-03-07 18:46:00 +0530

    Add project files.

---
+++ Core-Crud/Controllers/CategoryController1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core_Crud.Models;
+using Core_Crud.Data;
+
+namespace Core_Crud.Controllers
+{
+    public class CategoryController1 : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController1(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<category> objlist = _db.Category;
+            return View(objlist);
+        }
+    }
+}
---
+++ Core-Crud/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Threading.Tasks;
+using Core_Crud.Models;
+
+namespace Core_Crud.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+   
+        }
+        public DbSet<category> Category { get; set; }
+    }
+}
